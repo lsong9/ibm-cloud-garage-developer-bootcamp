42974e4e85328ed0e3609dbae6afd8d0d3fd575f
Lin Song <lsong9@users.noreply.github.com>
2017-07-28 15:45:34 -0500

    make other tests pass

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -2,8 +2,9 @@ describe.only('the palindrome canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true();
   });
-  function isPalindrome(word) {
-    return word === word.split('').reverse().join('');
+  function isPalindrome(phrase) {
+    if (phrase.trim().length === 0) return false;
+    return phrase === phrase.split('').reverse().join('');
   }
   describe('a palindrome', () => {
     it('is racecar', () => {
@@ -12,9 +13,17 @@ describe.only('the palindrome canary spec', () => {
     it('is not racecars', () => {
       isPalindrome('racecars').should.be.false();
     });
-    it('is not race car');
-    it('"   " is not a palindrome');
-    it('"" is not a palindrome');
-    it('is "mom dad mom"');
+    it('is not race car', () => {
+      isPalindrome('race car').should.be.false();
+    });
+    it('"   " is not a palindrome', () => {
+      isPalindrome('   ').should.be.false();
+    });
+    it('"" is not a palindrome', () => {
+      isPalindrome('').should.be.false();
+    });
+    it('is "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
   });
 });
