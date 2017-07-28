da39b86e4a51483469d8dd197b51b7eb2792d0f0
Lin Song <lsong9@users.noreply.github.com>
2017-07-28 15:18:54 -0500

    make 2nd test pass

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -2,14 +2,16 @@ describe.only('the palindrome canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true();
   });
-  function isPalindrome() {
-    return true;
+  function isPalindrome(word) {
+    return word === word.split('').reverse().join('');
   }
   describe('a palindrome', () => {
     it('is racecar', () => {
       isPalindrome('racecar').should.be.true();
     });
-    it('is not racecars');
+    it('is not racecars', () => {
+      isPalindrome('racecars').should.be.false();
+    });
     it('is not race car');
     it('"   " is not a palindrome');
     it('"" is not a palindrome');
