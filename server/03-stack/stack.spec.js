b187decae637122a35e4bb5dcbf33530ea1ab201
Lin Song <lsong9@users.noreply.github.com>
2017-07-29 19:46:36 -0500

    make stack test pass - 1

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,3 +1,37 @@
-describe('the stack spec', () => {
+const makeStack = () => {
+  let length = 0;
+  return {
+    isEmpty: () => length === 0,
+    push: () => length++,
+    size: () => length
+  };
+};
 
+let stack;
+
+describe.only('the stack spec', () => {
+  beforeEach(() => {
+    stack = makeStack();
+  });
+  it('starts empty', () => {
+    stack.isEmpty().should.be.true();
+  });
+  it('starts with stack size 0', () => {
+    stack.size().should.equal(0);
+  });
+  it('is not empty when pushed', () => {
+    stack.push(4);
+    stack.isEmpty().should.be.false();
+  });
+  it('leaves stack size 1 when pushed', () => {
+    stack.push();
+    stack.size().should.equal(1);
+  });
+  it('leaves stack empty when pushed and popped');
+  it('leaves stack size 0 when pushed and popped');
+  it('overflows');
+  it('under-flows');
+  it('pops the same one pushed');
+  it('pops the same two pushed');
+  it('accepts only positive capacity');
 });
