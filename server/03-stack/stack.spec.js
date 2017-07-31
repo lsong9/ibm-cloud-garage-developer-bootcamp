8674e2271795e8fa12fc354e51291204bcc33b08
Lin Song <lsong9@users.noreply.github.com>
2017-07-31 12:33:41 -0500

    make other stack tests pass

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,9 +1,26 @@
 const makeStack = () => {
-  let size = 0;
+  let queue = [];
+  let capacity = 3;
+  const setCapacity = (value) => {
+    if (value < 0) throw new Error('accepts only positive capacity');
+    capacity = value;
+  };
+  const isEmpty = () => queue.length === 0;
+  const size = () => queue.length;
+  const push = (element) => {
+    if (queue.length === capacity) throw new Error('Cannot exceed capacity');
+    queue.push(element);
+  };
+  const pop = () => {
+    if (queue.length === 0) throw new Error('Cannot pop when stack is empty');
+    return queue.pop();
+  };
   return {
-    isEmpty: () => size === 0,
-    push: () => size++,
-    size: () => size
+    isEmpty,
+    setCapacity,
+    size,
+    push,
+    pop
   };
 };
 
@@ -28,11 +45,42 @@ describe.only('the stack spec', () => {
     stack.push();
     stack.size().should.equal(1);
   });
-  it('leaves stack empty when pushed and popped');
-  it('leaves stack size 0 when pushed and popped');
-  it('overflows');
-  it('under-flows');
-  it('pops the same one pushed');
-  it('pops the same two pushed');
-  it('accepts only positive capacity');
+  it('leaves stack empty when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    stack.isEmpty().should.be.true();
+  });
+  it('leaves stack size 0 when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    stack.size().should.equal(0);
+  });
+  it('overflows', () => {
+    stack.push();
+    stack.push();
+    stack.push();
+    (() => {
+      stack.push();
+    }).should.throw('Cannot exceed capacity');
+  });
+  it('under-flows', () => {
+    (() => {
+      stack.pop();
+    }).should.throw('Cannot pop when stack is empty');
+  });
+  it('pops the same one pushed', () => {
+    stack.push('AAA');
+    stack.pop().should.equal('AAA');
+  });
+  it('pops the same two pushed', () => {
+    stack.push('AAA');
+    stack.push('BBB');
+    stack.pop().should.equal('BBB');
+    stack.pop().should.equal('AAA');
+  });
+  it('accepts only positive capacity', () => {
+    (() => {
+      stack.setCapacity(-3);
+    }).should.throw('accepts only positive capacity');
+  });
 });
