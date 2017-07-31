7582095a467f1a4d68a329748dbd4dc0fe8f48b3
Lin Song <lsong9@users.noreply.github.com>
2017-07-31 11:05:14 -0500

    make stack tests 3 and 4 pass

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,9 +1,18 @@
-let stack = {
-  isEmpty: () => true,
-  size: () => 0
+const makeStack = () => {
+  let size = 0;
+  return {
+    isEmpty: () => size === 0,
+    push: () => size++,
+    size: () => size
+  };
 };
 
+let stack;
+
 describe.only('the stack spec', () => {
+  beforeEach(() => {
+    stack = makeStack();
+  });
   it('starts empty', () => {
     stack.isEmpty().should.be.true();
   });
@@ -11,8 +20,14 @@ describe.only('the stack spec', () => {
   it('starts with stack size 0', () => {
     stack.size().should.equal(0);
   });
-  it('is not empty when pushed');
-  it('leaves stack size 1 when pushed');
+  it('is not empty when pushed', () => {
+    stack.push();
+    stack.isEmpty().should.be.false();
+  });
+  it('leaves stack size 1 when pushed', () => {
+    stack.push();
+    stack.size().should.equal(1);
+  });
   it('leaves stack empty when pushed and popped');
   it('leaves stack size 0 when pushed and popped');
   it('overflows');
