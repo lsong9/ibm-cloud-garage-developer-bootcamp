6d14d4d977cb843417f80a2b22453a175915dabc
Lin Song <lsong9@users.noreply.github.com>
2017-08-04 21:48:49 -0500

    make first test pass

--- server/00-canary/canary.spec.js
+++ server/00-canary/canary.spec.js
@@ -1,5 +1,5 @@
 describe('the canary spec', () => {
-  it.only('shows the infrastructure works', () => {
-    true.should.be.true('custom message');
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
   });
 });
